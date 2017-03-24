968f5b9b5568ba03f548b3e632bdaeb47d852816
charizard8888 <omkumr082001@outlook.com>
2017-03-24 16:29:32 +0530

    Create setdex_xy.js

---
+++ js/data/setdex_xy.js
@@ -0,0 +1,48 @@
+var SETDEX_SM = {
+	"Absol": {
+		"ORAS RU All-Out Attacker": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Adamant","ability":"Super Luck","item":"Life Orb","moves":["Knock Off","Sucker Punch","Superpower","Play Rough"]},
+		"ORAS RU Swords Dance": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Adamant","ability":"Super Luck","item":"Life Orb","moves":["Swords Dance","Knock Off","Sucker Punch","Superpower"]},
+		"ORAS UU Swords Dance": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Jolly","ability":"Justified","item":"Absolite","moves":["Swords Dance","Knock Off","Sucker Punch","Superpower"]},
+		"ORAS UU All-out Attacker": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Jolly","ability":"Justified","item":"Absolite","moves":["Knock Off","Sucker Punch","Pursuit","Superpower"]},
+	},
+	"Absol-Mega": {
+		"UU Showdown Usage": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Jolly","ability":"Magic Bounce","item":"Absolite","moves":["Knock Off","Sucker Punch","Swords Dance","Superpower"]},
+		"ORAS UU Swords Dance": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Jolly","ability":"Justified","item":"Absolite","moves":["Swords Dance","Knock Off","Sucker Punch","Superpower"]},
+		"ORAS UU All-out Attacker": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Jolly","ability":"Justified","item":"Absolite","moves":["Knock Off","Sucker Punch","Pursuit","Superpower"]},
+	},
+	"Charizard-Mega-X": {
+		"Offensive": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Jolly","ability":"Tough Claws","item":"Charizardite X","moves":["Dragon Dance","Outrage","Flare Blitz","Earthquake"]},
+		"Defensive": {"level":100,"evs":{"sp":8,"hp":248,"def":252},"nature":"Bold","ability":"Tough Claws","item":"Charizardite X","moves":["Outrage","Flare Blitz","Will-O-Wisp","Counter"]},
+	},
+	"Charizard-Mega-Y": {
+		"Offensive": {"level":100,"evs":{"sd":4,"sa":252,"sp":252},"nature":"Timid","ability":"Drought","item":"Charizardite Y","moves":["Air Slash","Blast Burn","Solar Beam","Dragon Pulse"]},
+		"Def": {"level":100,"evs":{"sp":8,"sp":252,"def":252},"nature":"Modest","ability":"Drought","item":"Charizardite Y","moves":["Air Slash","Blast Burn","Solar Beam","Dragon Pulse"]},
+	},
+	"Kyurem-Black": {
+		"Choice Scarf": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Naive","ability":"Teravolt","item":"Choice Scarf","moves":["Outrage", "Fusion Bolt", "Iron Head", "Ice Beam"]},
+		"Choice Band": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Naive","ability":"Teravolt","item":"Choice Band","moves":["Outrage", "Fusion Bolt", "Iron Head", "Ice Beam"]},
+	},
+	"Gyarados-Mega": {
+		"Standard": {"level":100,"evs":{"hp":88,"at":252,"sp":88,"def":80},"nature":"Adamant","ability":"Mold Breaker","item":"Gyaradosite","moves":["Waterfall","Crunch","Outrage","Earthquake"]},
+		"Bulky": {"level":100,"evs":{"hp":104,"at":196,"sd":8,"sp":100,"def":100},"nature":"Adamant","ability":"Mold Breaker","item":"Gyaradosite","moves":["Waterfall","Crunch","Outrage","Earthquake"]},
+	},
+	"Porygon-Z": {
+		"Standard Scarf": {"level":100,"evs":{"def":4,"sa":252,"sp":252},"nature":"Timid","ability":"Adaptibility","item":"Choice Scarf","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
+		"Bulky Scarf": {"level":100,"evs":{"def":240,"sa":172,"sp":96},"nature":"Timid","ability":"Adaptibility","item":"Choice Scarf","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
+		"Choice Specs": {"level":100,"evs":{"sa":252,"hp":252,"def":4},"nature":"Timid","ability":"Adaptibility","item":"Choice Scarf","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
+	},
+  "Latios": {
+		"OU Life Orb Attacker": {"level":100,"evs":{"df":4,"sp":252,"sa":252},"ivs":{"hp":29},"nature":"Timid","ability":"Levitate","item":"Life Orb","moves":["Draco Meteor","Psyshock","Recover","Defog"]},
+		"OU Choice Specs": {"level":100,"evs":{"sd":4,"sp":252,"sa":252},"nature":"Timid","ability":"Levitate","item":"Choice Specs","moves":["Draco Meteor","Psychic","Surf","Trick"]},
+		"OU Calm Mind": {"level":100,"evs":{"df":4,"sp":252,"sa":252},"ivs":{"hp":29},"nature":"Timid","ability":"Levitate","item":"Life Orb","moves":["Draco Meteor","Psyshock","Calm Mind","Recover"]},
+		"OU Choice Scarf": {"level":100,"evs":{"hp":4,"sp":252,"sa":252},"nature":"Timid","ability":"Levitate","item":"Choice Scarf","moves":["Draco Meteor","Psyshock","Defog","Trick"]},
+		"Uber Offensive Defog": {"level":100,"evs":{"hp":4,"sp":252,"sa":252},"nature":"Modest","ability":"Levitate","item":"Soul Dew","moves":["Defog","Draco Meteor","Psyshock","Roost"]},
+		"Uber 4 Attacks": {"level":100,"evs":{"hp":4,"sp":252,"sa":252},"nature":"Modest","ability":"Levitate","item":"Soul Dew","moves":["Draco Meteor","Psyshock","Grass Knot","Hidden Power Fire"]},
+		"Uber Calm Mind": {"level":100,"evs":{"hp":4,"sp":252,"sa":252},"nature":"Modest","ability":"Levitate","item":"Soul Dew","moves":["Calm Mind","Draco Meteor","Psyshock","Roost"]},
+		"Doubles Special Attacker": {"level":100,"evs":{"sd":4,"sp":252,"sa":252},"nature":"Timid","ability":"Levitate","item":"Life Orb","moves":["Draco Meteor","Psyshock","Tailwind","Protect"]},
+		"Battle Spot Doubles Special Attacker": {"level":50,"evs":{"hp":4,"sp":252,"sa":252},"nature":"Timid","ability":"Levitate","item":"Life Orb","moves":["Draco Meteor","Psyshock","Tailwind","Protect"]},
+		"Battle Spot Doubles Choice Specs": {"level":50,"evs":{"hp":4,"sp":252,"sa":252},"nature":"Timid","ability":"Levitate","item":"Choice Specs","moves":["Draco Meteor","Psyshock","Dragon Pulse","Trick"]},
+		"CAP Life Orb": {"level":100,"evs":{"sd":4,"sp":252,"sa":252},"nature":"Timid","ability":"Levitate","item":"Life Orb","moves":["Draco Meteor","Psyshock","Hidden Power Fire","Defog"]},
+		"CAP Calm Mind": {"level":100,"evs":{"sd":4,"sp":252,"sa":252},"nature":"Timid","ability":"Levitate","item":"Life Orb","moves":["Calm Mind","Draco Meteor","Psyshock","Roost"]},
+	},
+};
