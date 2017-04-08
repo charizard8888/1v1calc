a8306b4fcb8cb3cbd333abc15d490d10fcbaba7a
charizard8888 <omkumr082001@outlook.com>
2017-04-08 11:51:27 +0530

    Update setdex_sm.js

--- js/data/setdex_sm.js
+++ js/data/setdex_sm.js
@@ -1,15 +1,15 @@
 var SETDEX_SM = {
 	"Charizard": {
 		"1v1 Pre-Mega Off Zard X": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Jolly","ability":"Tough Claws","item":"Charizardite X","moves":["Dragon Dance","Outrage","Flare Blitz","Earthquake"]},
-		"1v1 Pre-Mega Defensive Zard X": {"level":100,"evs":{"sp":8,"hp":248,"def":252},"nature":"Bold","ability":"Tough Claws","item":"Charizardite X","moves":["Outrage","Flare Blitz","Will-O-Wisp","Counter"]},
+		"1v1 Pre-Mega Dfensive Zard X": {"level":100,"evs":{"sp":8,"hp":248,"df":252},"nature":"Bold","ability":"Tough Claws","item":"Charizardite X","moves":["Outrage","Flare Blitz","Will-O-Wisp","Counter"]},
 	},
 	"Charizard-Mega-X": {
 		"1v1 Offensive": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Jolly","ability":"Tough Claws","item":"Charizardite X","moves":["Dragon Dance","Outrage","Flare Blitz","Earthquake"]},
-		"1v1 Defensive": {"level":100,"evs":{"sp":8,"hp":248,"def":252},"nature":"Bold","ability":"Tough Claws","item":"Charizardite X","moves":["Outrage","Flare Blitz","Will-O-Wisp","Counter"]},
+		"1v1 Dfensive": {"level":100,"evs":{"sp":8,"hp":248,"df":252},"nature":"Bold","ability":"Tough Claws","item":"Charizardite X","moves":["Outrage","Flare Blitz","Will-O-Wisp","Counter"]},
 	},
 	"Charizard-Mega-Y": {
 		"1v1 Offensive": {"level":100,"evs":{"sd":4,"sa":252,"sp":252},"nature":"Timid","ability":"Drought","item":"Charizardite Y","moves":["Air Slash","Blast Burn","Solar Beam","Dragon Pulse"]},
-				"1v1 Def": {"level":100,"evs":{"sp":8,"sp":252,"def":252},"nature":"Modest","ability":"Drought","item":"Charizardite Y","moves":["Air Slash","Blast Burn","Solar Beam","Dragon Pulse"]},
+				"1v1 Df": {"level":100,"evs":{"sp":8,"sp":252,"df":252},"nature":"Modest","ability":"Drought","item":"Charizardite Y","moves":["Air Slash","Blast Burn","Solar Beam","Dragon Pulse"]},
 	},
 	"Kyurem-Black": {
 		"1v1 Choice Scarf": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Naive","ability":"Teravolt","item":"Choice Scarf","moves":["Outrage", "Fusion Bolt", "Iron Head", "Ice Beam"]},
@@ -18,20 +18,20 @@ var SETDEX_SM = {
 		"1v1 Bulky Icium Z": {"level":100,"evs":{"at":252,"hp":248,"spe":8},"nature":"Adamant","ability":"Teravolt","item":"Icium Z","moves":["Outrage", "Fusion Bolt", "Iron Head", "Freeze Shock"]},
 	},
 	"Gyarados": {
-		"1v1 Pre-Mega Standard": {"level":100,"evs":{"hp":88,"at":252,"sp":88,"def":80},"nature":"Adamant","ability":"Mold Breaker","item":"Gyaradosite","moves":["Waterfall","Crunch","Outrage","Earthquake"]},
-		"1v1 Pre-Mega Bulky": {"level":100,"evs":{"hp":104,"at":196,"sd":8,"sp":100,"def":100},"nature":"Adamant","ability":"Mold Breaker","item":"Gyaradosite","moves":["Waterfall","Crunch","Outrage","Earthquake"]},
+		"1v1 Pre-Mega Standard": {"level":100,"evs":{"hp":88,"at":252,"sp":88,"df":80},"nature":"Adamant","ability":"Mold Breaker","item":"Gyaradosite","moves":["Waterfall","Crunch","Outrage","Earthquake"]},
+		"1v1 Pre-Mega Bulky": {"level":100,"evs":{"hp":104,"at":196,"sd":8,"sp":100,"df":100},"nature":"Adamant","ability":"Mold Breaker","item":"Gyaradosite","moves":["Waterfall","Crunch","Outrage","Earthquake"]},
 	},
 	"Gyarados-Mega": {
-		"1v1 Standard": {"level":100,"evs":{"hp":88,"at":252,"sp":88,"def":80},"nature":"Adamant","ability":"Mold Breaker","item":"Gyaradosite","moves":["Waterfall","Crunch","Outrage","Earthquake"]},
-		"1v1 Bulky": {"level":100,"evs":{"hp":104,"at":196,"sd":8,"sp":100,"def":100},"nature":"Adamant","ability":"Mold Breaker","item":"Gyaradosite","moves":["Waterfall","Crunch","Outrage","Earthquake"]},
+		"1v1 Standard": {"level":100,"evs":{"hp":88,"at":252,"sp":88,"df":80},"nature":"Adamant","ability":"Mold Breaker","item":"Gyaradosite","moves":["Waterfall","Crunch","Outrage","Earthquake"]},
+		"1v1 Bulky": {"level":100,"evs":{"hp":104,"at":196,"sd":8,"sp":100,"df":100},"nature":"Adamant","ability":"Mold Breaker","item":"Gyaradosite","moves":["Waterfall","Crunch","Outrage","Earthquake"]},
 	},
 	"Magearna": {
-		"1v1 Choice Specs": {"level":100,"evs":{"def":4,"sa":252,"hp":252},"nature":"Modest","ability":"Soul-Heart","item":"Choice Specs","moves":["Fleur Cannon","Aura Sphere","Ice Beam","Energy Ball"]},
+		"1v1 Choice Specs": {"level":100,"evs":{"df":4,"sa":252,"hp":252},"nature":"Modest","ability":"Soul-Heart","item":"Choice Specs","moves":["Fleur Cannon","Aura Sphere","Ice Beam","Energy Ball"]},
 	},
 	"Porygon-Z": {
-		"1v1 Standard Scarf": {"level":100,"evs":{"def":4,"sa":252,"sp":252},"nature":"Timid","ability":"Adaptibility","item":"Choice Scarf","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
-		"1v1 Bulky Scarf": {"level":100,"evs":{"def":240,"sa":172,"sp":96},"nature":"Timid","ability":"Adaptibility","item":"Choice Scarf","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
-		"1v1 Choice Specs": {"level":100,"evs":{"sa":252,"hp":252,"def":4},"nature":"Timid","ability":"Adaptibility","item":"Choice Scarf","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
-		"1v1 Breakneck Blitz": {"level":100,"evs":{"def":4,"sa":252,"sp":252},"nature":"Timid","ability":"Adaptibility","item":"Normalium Z","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
+		"1v1 Standard Scarf": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Timid","ability":"Adaptibility","item":"Choice Scarf","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
+		"1v1 Bulky Scarf": {"level":100,"evs":{"df":240,"sa":172,"sp":96},"nature":"Timid","ability":"Adaptibility","item":"Choice Scarf","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
+		"1v1 Choice Specs": {"level":100,"evs":{"sa":252,"hp":252,"df":4},"nature":"Timid","ability":"Adaptibility","item":"Choice Scarf","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
+		"1v1 Breakneck Blitz": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Timid","ability":"Adaptibility","item":"Normalium Z","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
 	},
 };
