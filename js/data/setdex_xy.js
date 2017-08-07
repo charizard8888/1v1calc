e0467e331ce289eecad30b77089669fc2a35e422
charizard8888 <omkumr082001@outlook.com>
2017-08-07 13:27:07 +0530

    Update setdex_xy.js

--- js/data/setdex_xy.js
+++ js/data/setdex_xy.js
@@ -10,4 +10,43 @@ var SETDEX_XY = {
 	"Charizard-Mega-Y": {
 		"1v1 Sun Attacker": {"level":100,"evs":{"sd":4,"sa":252,"sp":252},"nature":"Timid","ability":"Drought","item":"Charizardite Y","moves":["Air Slash","Blast Burn","Solar Beam","Ancient Power"]},
 	},
+	"Kyurem-Black": {
+		"1v1 Choice Scarf": {"level":100,"evs":{"sa":4,"at":252,"sp":252},"nature":"Naive","ability":"Teravolt","item":"Choice Scarf","moves":["Outrage", "Fusion Bolt", "Earth Power", "Ice Beam"]},
+		"1v1 Choice Specs": {"level":100,"evs":{"sd":4,"sa":252,"sp":252},"nature":"Timid","ability":"Teravolt","item":"Choice Specs","moves":["Draco Meteor", "Ice Beam", "Earth Power", "Flash Cannon"]},
+		"1v1 Offensive Haban Berry": {"level":100,"evs":{"sa":4,"at":252,"sp":252},"nature":"Naughty","ability":"Teravolt","item":"Haban Berry","moves":["Outrage", "Fusion Bolt", "Flash Cannon", "Ice Beam"]},
+		"1v1 Bulky Weakness Policy": {"level":100,"evs":{"at":28,"hp":212,"df":252,"sd":16},"nature":"Relaxed","ability":"Teravolt","item":"Weakness Policy","moves":["Outrage", "Fusion Bolt", "Ice Beam", "Earth Power"]},
+	},
+	"Mawile-Mega": {
+		"1v1 Bulky Offense": {"level":100,"evs":{"at":252,"hp":248,"sp":8},"nature":"Adamant","ability":"Huge Power","item":"Mawilite","moves":["Play Rough","Iron Head","Sucker Punch","Fire Fang"]},
+	},
+	"Porygon-Z": {
+		"1v1 Standard Scarf": {"level":100,"evs":{"sd":4,"sa":252,"sp":252},"nature":"Modest","ability":"Adaptability","item":"Choice Scarf","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
+		"1v1 Bulky Scarf": {"level":100,"evs":{"df":240,"sa":172,"sp":96},"nature":"Timid","ability":"Adaptability","item":"Choice Scarf","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
+		"1v1 Bulky Specs": {"level":100,"evs":{"sa":132,"hp":252,"df":124},"nature":"Modest","ability":"Adaptability","item":"Choice Specs","moves":["Hyper Beam", "Uproar", "Shadow Ball", "Ice Beam"]},
+	},
+	"Pinsir-Mega": {
+		"1v1 Hyper Cutter": {"level":100,"evs":{"at":252,"df":40,"sp":216},"nature":"Jolly","ability":"Aerilate","item":"Pinsirite","moves":["Giga Impact","Close Combat","Quick Attack","Earthquake"]},
+		"1v1 Mold Breaker": {"level":100,"evs":{"at":252,"hp":40,"sp":216},"nature":"Jolly","ability":"Aerilate","item":"Pinsirite","moves":["Giga Impact","Close Combat","Quick Attack","Earthquake"]},
+	},
+	"Greninja": {
+		"1v1 Choice Specs": {"level":100,"evs":{"sa":252,"sp":252,"sd":4},"nature":"Timid","ability":"Protean","item":"Choice Specs","moves":["Hydro Cannon","Grass Knot","Dark Pulse","Ice Beam"]},
+	},
+	"Meloetta": {
+		"1v1 Choice Specs": {"level":100,"evs":{"sa":252,"sp":8,"hp":248},"nature":"Modest","ability":"Serene Grace","item":"Choice Specs","moves":["Hyper Beam","Psychic","Shadow Ball","Trick"]},
+	},
+	"Genesect": {
+		"1v1 Choice Specs": {"level":100,"evs":{"sp":4,"sa":252,"hp":252},"nature":"Modest","ability":"Download","item":"Choice Specs","moves":["Bug Buzz","Flash Cannon","Thunderbolt","Flamethrower"]},
+		"1v1 Choice Scarf": {"level":100,"evs":{"at":4,"sa":252,"hp":252},"nature":"Hasty","ability":"Download","item":"Choice Scarf","moves":["Bug Buzz","Iron Head","Thunderbolt","Flamethrower"]},
+	},
+	"Dragonite": {
+		"1v1 Choice Band": {"level":100,"evs":{"at":252,"hp":252,"sp":4},"nature":"Adamant","ability":"Multiscale","item":"Choice Band","moves":["Outrage","Extreme Speed","Earthquake","Fire Punch"]},
+		"1v1 Weakness Policy": {"level":100,"evs":{"at":252,"sp":252,"sd":4},"nature":"Adamant","ability":"Multiscale","item":"Choice Band","moves":["Outrage","Extreme Speed","Earthquake","Fire Punch"]},
+	},
+	"Garchomp": {
+		"1v1 Choice Band": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Jolly","ability":"Rough Skin","item":"Choice Band","moves":["Outrage","Earthquake","Fire Fang","Poison Jab"]},
+		"1v1 Choice Scarf": {"level":100,"evs":{"sd":4,"at":252,"sp":252},"nature":"Jolly","ability":"Rough Skin","item":"Choice Scarf","moves":["Outrage","Earthquake","Fire Fang","Poison Jab"]},
+	},
+	"Jumpluff": {
+		"1v1 Leech Seed Stall": {"level":100,"evs":{"df":252,"sd":4,"sp":252},"nature":"Jolly","ability":"Infliltrator","item":"Wide Lens","moves":["Leech Seed","Sleep Powder","Substitute","Protect"]},
+	},
   };
